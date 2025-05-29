d436fa2cfb51ab31db9b87311293286368de9ffc
jazeel-hub <jazeelmohammed79@gmail.com>
2025-05-29 16:43:16 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 13426
+let accountEmail = "jazeel@gmail.com"
+var accountPassword = "12345"
+accountCity = "shiroor"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity ]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using nodeJs not browser
+console.log(3 
+    + 3); //code readability that should be high
+
+console.log("mohammed jazeel")
+
+
+let name = "mohammed jazeel"
+let age = 21
+let isLoggedIn = false
+
+// number => 20 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);

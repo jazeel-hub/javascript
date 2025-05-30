cad79079cc352fd4d4c87fd7e6966ee5808812be
jazeel-hub <jazeelmohammed79@gmail.com>
2025-05-30 02:12:27 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "jazeel"
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+let nnumberInValue = Number(score)
+// console.log(nnumberInValue)
+
+// "33 => 33"
+// "33lmn => NaN"
+//  true = 1; false => 0
+
+let isLoggedIn = "jazeel"
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "jazeel" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

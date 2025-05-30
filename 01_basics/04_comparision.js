5515422c93d0f8af6846c39ee44d94b879c0790b
jazeel-hub <jazeelmohammed79@gmail.com>
2025-05-30 02:55:08 +0000

    added

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,8 +8,8 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
+console.log(null > 0); // comparision operator will change convert the null value into number or NaN value
+console.log(null == 0); // equality operator will check as it is
 console.log(null >= 0);
 
 console.log(undefined == 0);
@@ -18,4 +18,4 @@ console.log(undefined < 0);
 
 // === 
 
-console.log("2" === 2);
+console.log("2" === 2); //strict check
